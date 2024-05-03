755da488408a9002128e8926fda006fd418d4e87
PulpFiction1234 <rafaelalbertobenguria@gmail.com>
2024-05-03 18:15:59 -0400

    Actualizar main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles.css'; 
@@ -8,4 +8,27 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+);
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './styles.css';
+ 
+// Renderiza la aplicación en el elemento con ID 'root'
+const rootElement = document.getElementById('root');
+ 
+if (!rootElement) {
+  throw new Error("El elemento con ID 'root' no existe en el DOM.");
+}
+ 
+ReactDOM.hydrate(
+<React.StrictMode>
+    {/* Comentarios explicativos */}
+    {/* El componente App es el componente principal de la aplicación */}
+    {/* StrictMode activa verificaciones adicionales y advertencias en el modo de desarrollo */}
+<App />
+</React.StrictMode>,
+  rootElement
 );
